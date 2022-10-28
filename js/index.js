758aa2fcc22f3df38439caeb5f8498f40f8abcac
EllaGospel <ella4real5@gmail.com>
2022-10-28 12:43:22 +0100

    new signup again

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const togglePassword=document.querySelector('#togglePassword');
+const password=document.querySelector('#Password');
+
+togglePassword.addEventListener('click',function (e) {
+
+const type=password.getAttribute("type") ==="password" ?"text" : "Password";
+password.getAttribute("type",type);
+
+this.classList.toggle("fa-eye-low-vision")
+
+
+
+});
