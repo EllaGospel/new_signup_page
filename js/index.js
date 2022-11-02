60217aca76255519bd0bfc244eb853c7f0be407d
EllaGospel <ella4real5@gmail.com>
2022-11-02 15:25:01 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
+
+function disappear(){
+    const divForm=document.querySelector('click_btn')
+    divForm.style.display.none
+}
+
+
+const formitself= document.getElementById('form')
+formitself.addEventListener("click", disappear)
+
+
+
+
+
+
+
+
+
 function toggle (){
-    const eyefield = document.querySelector('#Password');
-    const type =eyefield.getAttribute('type') === "password" ? "type" : "password"
-   console.log('type')
+    const eyefield  = document.querySelector('#Password');
+    const password =eyefield.getAttribute('type') === "password" ? "type" : "password"
+
     eyefield.setAttibute('type', type) 
 }
 
 
-const Password =document.getElementById('togglePassword');
+const togglePassword =document.getElementbyId('togglePassword');
 Password.addEventListener('click', toggle)
 
 this.classList.toggle('fa-eye')
