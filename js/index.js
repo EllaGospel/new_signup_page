5a062afda91eab4d7b72e2fa01cdef83618e0e13
EllaGospel <ella4real5@gmail.com>
2022-11-02 14:25:34 +0100

    other changes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 function toggle (){
     const eyefield = document.querySelector('#togglePassword')
     const type =eyefield.getAttribute('type') === "password" ? "type" : "password"
-   
+   console.log('type')
     eyefield.setAttibute('type', type) 
-
-
 }
 
 
-const Password =document.getElementById(togglePassword)
+const Password =document.getElementById('togglePassword')
 Password.addEventListener('click', toggle)
+
+this.classList.toggle('fa-eye')
+
+
 
 
