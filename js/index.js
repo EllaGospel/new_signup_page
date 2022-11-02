4843101639cf5e4740e08b2ad030f53b63a26d0e
EllaGospel <ella4real5@gmail.com>
2022-11-02 14:47:19 +0100

    correct one

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 function toggle (){
-    const eyefield = document.querySelector('#togglePassword')
+    const eyefield = document.querySelector('#Password');
     const type =eyefield.getAttribute('type') === "password" ? "type" : "password"
    console.log('type')
     eyefield.setAttibute('type', type) 
 }
 
 
-const Password =document.getElementById('togglePassword')
+const Password =document.getElementById('togglePassword');
 Password.addEventListener('click', toggle)
 
 this.classList.toggle('fa-eye')
