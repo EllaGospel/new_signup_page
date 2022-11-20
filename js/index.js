b665d6e51c4789763f54e3d65e1665d5e4d01c72
EllaGospel <ella4real5@gmail.com>
2022-11-20 15:13:26 +0100

    new ones

--- js/index.js
+++ js/index.js
@@ -15,19 +15,6 @@ setInterval(clock, 1000);
 
 
 
-function toggle(e){
-    const eyefield=document.querySelector('#${e.target.id}').nextElementSibling;
-    const password=eyefield.getAttribute('type') === 'password' ? 'text':'password';
-    eyefield.setAttribute('type',password);
-
-}
-
-
-const togglepassword=document.getElementById('togglePassword');
-const togglepassword1= document.getElementById('togglepassword1');
-togglepassword.addEventListener('click',toggle);
-togglepassword1.addEventListener('click',toggle);
-
 
 
 
