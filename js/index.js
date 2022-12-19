912416adbb72765b7e88f03825c694b9a8a14f78
EllaGospel <ella4real5@gmail.com>
2022-12-19 18:36:01 +0100

    some changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+function togglepassword(e){
+    const buttontarget=e.target.parentNode;
+    
+    const type=buttontarget.previousElementSibling.getAttribute("type");
+    if (type==="password"){
+        buttontarget.previousElementSibling.setAttribute("type", "text");
+        buttontarget.setAttribute("data-password-lock","isunlocked");
+       return
+    }
+    buttontarget.previousElementSibling.setAttribute("type", "password");
+    buttontarget.setAttribute("data-password-lock","islocked");
+
+}
+
+
+const passwordButton=document.querySelectorAll('.password_btn')
+passwordButton.forEach((btn) => btn.addEventListener('click',togglepassword));
+
+
+
+
+
+
+
+
+
+
 options = {
     year: 'numeric', month:'long', day:'numeric',
     hour:'numeric', minute:'numeric', second: 'numeric'
@@ -7,6 +34,17 @@ options = {
 const clock= () => dateTime.innerText=new Intl.DateTimeFormat('en-EN', options).format(new Date())
 clock()
 setInterval(clock, 1000);
+
+
+
+
+function Onsubmithandler(eventobject){
+    Onsubmithandler.preventDefault();
+}
+
+
+
+document.querySelector('#mainform').addEventListener('sumbit',Onsubmithandler)
 
 
 
